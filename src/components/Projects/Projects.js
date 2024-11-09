05b01325939e0611ef7acf868f4b79994278bad7
integralnana <nwpscw79@gmail.com>
2024-11-10 03:23:37 +0700

    add Pics in Projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -25,6 +25,47 @@ const ProjectCard = ({ project, onClick, index }) => (
   </div>
 );
 
+const ImageGallery = ({ images }) => {
+  const [selectedImage, setSelectedImage] = useState(0);
+  const [isFullscreen, setIsFullscreen] = useState(false);
+
+  const toggleFullscreen = () => setIsFullscreen(!isFullscreen);
+
+  return (
+    <div className="image-gallery">
+      <div className="image-container">
+        <div className="main-image" onClick={toggleFullscreen}>
+          <img
+            src={images[selectedImage]}
+            alt={`Screenshot ${selectedImage + 1}`}
+          />
+        </div>
+      </div>
+      <div className="thumbnail-container">
+        {images.map((image, index) => (
+          <div
+            key={index}
+            className={`thumbnail ${selectedImage === index ? "active" : ""}`}
+            onClick={() => setSelectedImage(index)}
+          >
+            <img src={image} alt={`Thumbnail ${index + 1}`} />
+          </div>
+        ))}
+      </div>
+      {isFullscreen && (
+        <div className="fullscreen-overlay" onClick={toggleFullscreen}>
+          <img
+            src={images[selectedImage]}
+            alt={`Fullscreen ${selectedImage + 1}`}
+            className="fullscreen-image"
+            onClick={(e) => e.stopPropagation()}
+          />
+        </div>
+      )}
+    </div>
+  );
+};
+
 const Projects = () => {
   const projectList = [
     {
@@ -34,6 +75,13 @@ const Projects = () => {
       content:
         "• Developed a mobile application using Flutter and Firebase for users to share product purchases.<br /><br />• Integrated Google Maps API to provide location-based services and enhance user experience.<br /><br />• Implemented real-time database and authentication using Firebase, ensuring data security and user management.<br /><br />• Built an admin dashboard using React for managing users, products, and transactions.<br /><br />• https://github.com/integralnana/Project_Mobileapp.git",
       skills: ["Flutter", "Firebase", "React", "Google Maps API"],
+      images: [
+        "../Project_pics/p_home.png",
+        "../Project_pics/p_Login.png",
+        "../Project_pics/p_sharescreen.png",
+        "../Project_pics/p_createpost.png",
+        "../Project_pics/p_chat.png",
+      ],
     },
     {
       name: "Food Recommendation App",
@@ -42,6 +90,17 @@ const Projects = () => {
       content:
         "• Played a key role as the primary coder in a team-based project for a Software Engineer course.<br /><br />• Responsible for front-end development using Flutter Framework to create interactive and interfaces.<br /><br />• Implementing and maintaining back-end functionality using PHP and MySQL to manage and store data efficiently.",
       skills: ["Flutter", "HTML", "CSS", "PHP", "MySQL"],
+      images: [
+        "../Project_pics/f_splash.png",
+        "../Project_pics/f_home.png",
+        "../Project_pics/f_login.png",
+        "../Project_pics/f_register.png",
+        "../Project_pics/f_ins.png",
+        "../Project_pics/f_addfood.png",
+        "../Project_pics/f_search.png",
+        "../Project_pics/f_searching.png",
+        "../Project_pics/f_post.png",
+      ],
     },
     {
       name: "Webboard Development",
@@ -50,6 +109,14 @@ const Projects = () => {
       content:
         "• Designed and developed a responsive website using HTML, CSS, and JavaScript to enhance user experience across devices.<br /><br />• Implemented user authentication and data handling with PHP, ensuring secure access and interaction.<br /><br />• Managed data retrieval and storage efficiently with MySQL, supporting dynamic content and user data management.",
       skills: ["HTML", "CSS", "JavaScript", "PHP", "MySQL"],
+      images: [
+        "../Project_pics/w_index.png",
+        "../Project_pics/w_login.png",
+        "../Project_pics/w_register.png",
+        "../Project_pics/w_createpost.png",
+        "../Project_pics/w_post.png",
+        "../Project_pics/w_postcomment.png",
+      ],
     },
   ];
 
@@ -100,16 +167,17 @@ const Projects = () => {
         ))}
       </div>
 
-      <Modal show={showModal} onHide={handleClose} centered>
+      <Modal show={showModal} onHide={handleClose} centered size="lg">
         <Modal.Header closeButton>
-          <Modal.Title>
-            {selectedProject?.name} ({selectedProject?.year})
-          </Modal.Title>
+          <Modal.Title>{selectedProject?.name}</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           {selectedProject && (
             <>
-              <div>{createContentWithLinks(selectedProject.content)}</div>
+              <ImageGallery images={selectedProject.images} />
+              <div className="mt-4">
+                {createContentWithLinks(selectedProject.content)}
+              </div>
               <div className="skills-container">
                 <div className="skills-list">
                   {selectedProject.skills.map((tech, index) => (
